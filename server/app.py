fcc2d4cbd1ebad6645060ab8d5176bd7d771b0d4
kibetbrian <brian.kibet2@student.moringaschool.com>
2023-09-26 15:14:19 +0300

    lab completed

--- server/app.py
+++ server/app.py
@@ -1,12 +1,9 @@
-# server/app.py
-
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
-from models import db, User, Review, Game
+from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -19,40 +16,63 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    return "Index for Game/Review/User API"
+    return '<h1>Bakery GET API</h1>'
 
-@app.route('/games')
-def games():
-
-    games = []
-    for game in Game.query.all():
-        game_dict = {
-            "title": game.title,
-            "genre": game.genre,
-            "platform": game.platform,
-            "price": game.price,
+@app.route('/bakeries', methods=['GET'])
+def get_bakeries():
+    bakeries = Bakery.query.all()
+    bakery_list = []
+    for bakery in bakeries:
+        bakery_data = {
+            "id": bakery.id,
+            "name": bakery.name,
         }
-        games.append(game_dict)
+        bakery_list.append(bakery_data)
+    return jsonify(bakery_list)
 
-    response = make_response(
-        games,
-        200
-    )
+@app.route('/bakeries/<int:id>', methods=['GET'])
+def bakery_by_id(id):
+    bakery = Bakery.query.get(id)
 
-    return response
+    if bakery is None:
+        response = {"error": "Bakery not found"}
+        return jsonify(response), 404
 
-@app.route('/games/<int:id>')
-def game_by_id(id):
-    game = Game.query.filter(Game.id == id).first()
+    baked_goods = [baked_good.name for baked_good in bakery.baked_goods]
+
+    bakery_data = {
+        "id": bakery.id,
+        "name": bakery.name,
+        "baked_goods": baked_goods
+    }
+
+    return jsonify(bakery_data)
+
+@app.route('/baked_goods/by_price', methods=['GET'])
+def baked_goods_by_price():
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+
+    baked_goods_data = []
+    for baked_good in baked_goods:
+        baked_good_data = {
+            "id": baked_good.id,
+            "name": baked_good.name,
+            "price": baked_good.price,
+        }
+        baked_goods_data.append(baked_good_data)
     
-    game_dict = game.to_dict()
+    return jsonify(baked_goods_data)
 
-    response = make_response(
-        game_dict,
-        200
-    )
-
-    return response
+@app.route('/baked_goods/most_expensive', methods=['GET'])
+def most_expensive_baked_good():
+    most_expensive_baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if most_expensive_baked_good is None:
+        baked_good_data = {
+        "id": most_expensive_baked_good.id,
+        "name": most_expensive_baked_good.name,
+        "price": most_expensive_baked_good.price,
+        }
+    return jsonify({"message": "No baked goods found."}), 404
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
